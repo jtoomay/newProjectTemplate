cda9d3df0e01e00e5fac81113e609d6fcde9d6ec
Jake Toomay <jake@toomay.dev>
2023-02-26 13:02:55 -0600

    tw / twin.macro installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import tw, { styled } from 'twin.macro';
+import { useState } from 'react';
 
 function App() {
+
+  const [toggle, setToggle] = useState(false);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +15,22 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+       
+          <Test toggle={toggle}>Test Component</Test>
+          <TestButton onClick={() => setToggle(!toggle)}>Click me to test styledComponents</TestButton>
           Learn React
-        </a>
+      
       </header>
     </div>
   );
 }
 
 export default App;
+
+const TestButton = tw.button`text-white p-4 bg-blue-500 rounded-xl my-10`
+
+
+const Test = styled.p(({toggle}) => [
+  tw`text-red-500`,
+  toggle && tw`text-blue-500`,
+])
